ab29b89ee1e297bc1523ffe4d379042a0bdf4136
Joydeep Mukherjee <joydeepm02@gmail.com>
2023-12-25 17:59:06 -0800

    Open TCP connection to host

--- traceroute.c
+++ traceroute.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <sys/errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 void usage(void);
 
@@ -10,8 +16,33 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    int fd;
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
     const char *host = argv[1];
-    (void) host;
+    printf("traceroute to %s\n", host);
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int ok;
+    struct addrinfo* res;
+    if ((ok = getaddrinfo(host, "80", &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ok));
+        exit(1);
+    }
+
+    if (connect(fd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("connect");
+        exit(1);
+    }
+
+    freeaddrinfo(res);
 
     return 0;
 }
