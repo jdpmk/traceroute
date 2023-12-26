b8fbcefc05887e6126d64373ae5ca9564aedc8ba
Joydeep Mukherjee <joydeepm02@gmail.com>
2023-12-26 11:31:53 -0800

    Send probe byte and receive ICMP message

--- traceroute.c
+++ traceroute.c
@@ -3,9 +3,11 @@
 #include <string.h>
 
 #include <netdb.h>
+#include <netinet/ip_icmp.h>
 #include <sys/errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 void usage(void);
 
@@ -16,8 +18,12 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    int fd;
-    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    int sfd, rfd;
+    if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+    if ((rfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) {
         perror("socket");
         exit(1);
     }
@@ -31,23 +37,57 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
 
     int ok;
-    struct addrinfo* res;
+    struct addrinfo *res;
     if ((ok = getaddrinfo(host, "80", &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ok));
         exit(1);
     }
 
-    if (connect(fd, res->ai_addr, res->ai_addrlen) == -1) {
+    if (connect(sfd, res->ai_addr, res->ai_addrlen) == -1) {
         perror("connect");
         exit(1);
     }
 
     freeaddrinfo(res);
 
+    // TODO: perform the following for multiple hops
+    const int TTL = 1;
+    if (setsockopt(sfd, IPPROTO_IP, IP_TTL, &TTL, sizeof(TTL)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    char probe = '\0';
+    if (send(sfd, &probe, sizeof(probe), 0) == -1) {
+        perror("send");
+        exit(1);
+    }
+
+    char buf[512];
+    if (recv(rfd, buf, sizeof(buf), 0) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    struct ip *ip_hdr = (struct ip *) buf;
+    // IP header length in 32-bit words. See ip(4).
+    u_int ip_hlen = ip_hdr->ip_hl << 2;
+    struct icmp *icmp_hdr = (struct icmp *) (buf + ip_hlen);
+
+    u_char type = icmp_hdr->icmp_type;
+    u_char code = icmp_hdr->icmp_code;
+
+    if (type == ICMP_TIMXCEED && code == ICMP_TIMXCEED_INTRANS) {
+        // TODO: print time taken for hop
+    }
+
+    close(sfd);
+    close(rfd);
+
     return 0;
 }
 
 void usage(void)
 {
-    fprintf(stderr, "usage: traceroute [host]\n");
+    fprintf(stderr, "usage: [sudo] traceroute host\n");
 }
