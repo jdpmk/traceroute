c4877d27d91589cc6e34ffd3b6fbe571ac04e3a4
Joydeep Mukherjee <joydeepm02@gmail.com>
2023-12-26 17:24:24 -0500

    Send package for multiple hops

--- traceroute.c
+++ traceroute.c
@@ -53,39 +53,42 @@ int main(int argc, char **argv)
 
     freeaddrinfo(res);
 
-    // TODO: perform the following for multiple hops
-    const int TTL = 1;
-    if (setsockopt(sfd, IPPROTO_IP, IP_TTL, &TTL, sizeof(TTL)) == -1) {
-        perror("setsockopt");
-        exit(1);
-    }
+    const int MAX_TTL = 10;
+    for (int ttl = 1; ttl < MAX_TTL; ++ttl) {
+        if (setsockopt(sfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
 
-    char probe = '\0';
-    if (send(sfd, &probe, sizeof(probe), 0) == -1) {
-        perror("send");
-        exit(1);
-    }
-    double send_ms = time_now_ms();
+        char probe = '\0';
+        if (send(sfd, &probe, sizeof(probe), 0) == -1) {
+            perror("send");
+            exit(1);
+        }
+        double send_ms = time_now_ms();
 
-    char buf[512];
-    if (recv(rfd, buf, sizeof(buf), 0) == -1) {
-        perror("recv");
-        exit(1);
-    }
-    double recv_ms = time_now_ms();
+        char buf[512];
+        if (recv(rfd, buf, sizeof(buf), MSG_WAITALL) == -1) {
+            perror("recv");
+            exit(1);
+        }
+        double recv_ms = time_now_ms();
 
-    struct ip *ip_hdr = (struct ip *) buf;
-    // IP header length in 32-bit words. See ip(4).
-    u_int ip_hlen = ip_hdr->ip_hl << 2;
-    struct icmp *icmp_hdr = (struct icmp *) (buf + ip_hlen);
+        struct ip *ip_hdr = (struct ip *) buf;
+        // Convert IP header length from 32-bit words to bytes. See ip(4).
+        u_int ip_hlen = ip_hdr->ip_hl << 2;
+        struct icmp *icmp_hdr = (struct icmp *) (buf + ip_hlen);
 
-    u_char type = icmp_hdr->icmp_type;
-    u_char code = icmp_hdr->icmp_code;
+        u_char type = icmp_hdr->icmp_type;
+        u_char code = icmp_hdr->icmp_code;
 
-    if (type == ICMP_TIMXCEED && code == ICMP_TIMXCEED_INTRANS) {
-        const char *src_addr = inet_ntoa(ip_hdr->ip_src);
-        double rtt_ms = (recv_ms - send_ms) / 2;
-        printf("%s %f\n", src_addr, rtt_ms);
+        if (type == ICMP_TIMXCEED && code == ICMP_TIMXCEED_INTRANS) {
+            const char *src_addr = inet_ntoa(ip_hdr->ip_src);
+            double rtt_ms = (recv_ms - send_ms) / 2;
+            printf("%s %f\n", src_addr, rtt_ms);
+        }
+
+        // TODO: break when the host is reached
     }
 
     close(sfd);
